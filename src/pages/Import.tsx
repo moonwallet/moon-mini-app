d4c7260244d6618f0323f55bf66c99c5a49339b0
iower <iower@tutanota.com>
2024-05-20 01:12:31 +0500

    Generate keypairs

--- src/pages/Import.tsx
+++ src/pages/Import.tsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import { Buffer } from 'buffer'
 import { useState } from 'react'
 
+import { Keypair } from '@solana/web3.js'
+import * as bip39 from 'bip39'
+
+// @ts-ignore
+window.Buffer = Buffer
+
 function Import() {
-  const [mnemonic, setMnemonic] = useState<string>('')
+  const [mnemonic, setMnemonic] = useState<string>('test')
+
+  const seed = bip39.mnemonicToSeedSync(mnemonic, '') // mnemonic, password
+  const keypair = Keypair.fromSeed(seed.slice(0, 32))
+  const pubKey = keypair.publicKey.toBase58()
 
   return (
     <>
-      Import
-      <input
-        type="text"
+      <h2>Enter Recovery Phrase</h2>
+      <textarea
+        className="w-[300px] h-[100px] border border-black"
         value={mnemonic}
         onChange={(e) => { setMnemonic(e.target.value) }}
       />
+      <br />
+      <br />
+      <div>publicKey: {pubKey}</div>
+
+      <a href={`https://solscan.io/account/${pubKey}`}>Scaner</a>
     </>
   )
 }
