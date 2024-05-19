8657ff7a4e0817dd289e8c0e2b47658af340025d
iower <iower@tutanota.com>
2024-05-20 01:20:18 +0500

    Moon

--- src/pages/Setup.tsx
+++ src/pages/Setup.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
 import { useNavigate } from 'react-router-dom'
+
+import moon from '../assets/moon.png'
 
 function Create() {
   const navigate = useNavigate()
   return (
     <>
+      <div className="flex items-center justify-center w-full h-[300px]">
+        <img src={moon} className="w-[180px] h-[180px]" alt="React logo" />
+      </div>
       <div>
         <button onClick={() => { navigate('/import') }}>Import</button>
       </div>
+      <br />
       <div>
         <button onClick={() => { navigate('/create') }}>Create</button>
       </div>
