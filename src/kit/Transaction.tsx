00ff6b0f6403d3571c9acc3016f0152f342e345c
iower <iower@tutanota.com>
2024-05-25 01:29:24 +0500

    History draft

---
+++ src/kit/Transaction.tsx
@@ -0,0 +1,44 @@
+import cx from 'classnames'
+import { useMemo} from 'react'
+
+import format from '../format'
+
+type TTxType = 'received' | 'swapped' | 'sent'
+
+const Transaction = ({ }: { }) => {
+  const txTypes: TTxType[] = ['received', 'swapped', 'sent']
+  const txType: TTxType = txTypes[Math.floor(Math.random() * 3)]
+
+  const amount = useMemo(() => Math.random() * 100, [])
+  const amount2 = useMemo(() => Math.random() * 100, [])
+  return (
+    <div
+      className="Token flex items-center gap-2 bg-white py-[10px] px-[14px] rounded-[16px]"
+    >
+      <div className="w-[48px] h-[48px] bg-text/10 rounded-full" />
+      <div className="flex-1">
+        <div className="text-[18px] leading-[22px] font-medium capitalize">{txType}</div>
+        <div className="text-[14px] leading-[18px] text-[#3C3C4399]">
+          {txType === 'received' && `From ${format.address('00000000000000')}`}
+          {txType === 'sent' && `To ${format.address('00000000000000')}`}
+          {txType === 'swapped' && 'Moon Wallet'}
+        </div>
+      </div>
+      <div className="text-right">
+        <div className={cx(
+          'text-[17px] leading-[22px]',
+          txType === 'sent' ? 'text-[#3C3C4399]' : 'text-[#50AB19]'
+        )}>
+          {txType === 'sent' ? '−' : '+'}{format.amount(amount)} ABC
+        </div>
+        <div className={cx(
+          'text-[14px] leading-[18px] text-[#3C3C4399]'
+        )}>
+          {txType === 'swapped' && `−${format.amount(amount2)} SOL`}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Transaction
--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,11 +1,31 @@
 import Page from '../kit/Page'
 import Menu from '../kit/Menu'
+import Transaction from '../kit/Transaction'
 
 function History() {
 
+  const DateMark = ({ date }: { date: string }) => (
+    <div className="Date pt-10 pb-2 text-[14px] leading-[18px] text-[#3C3C4399]">{date}</div>
+  )
+
   return (
     <Page bottom={<Menu />}>
-      Soon...
+      <DateMark date={'16 May'} />
+      <div className="flex flex-col gap-2">
+        <Transaction />
+      </div>
+      <DateMark date={'15 May'} />
+      <div className="flex flex-col gap-2">
+        <Transaction />
+        <Transaction />
+        <Transaction />
+      </div>
+      <DateMark date={'14 May'} />
+      <div className="flex flex-col gap-2">
+        <Transaction />
+        <Transaction />
+        <Transaction />
+      </div>
     </Page>
   )
 }
