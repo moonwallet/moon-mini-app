e4a6fca81efacc5c40241c00a9bad972eead4a8e
iower <iower@tutanota.com>
2024-06-04 01:27:32 +0500

    watchlist

--- src/kit/Token.tsx
+++ src/kit/Token.tsx
@@ -12,7 +12,9 @@ const Token = ({ title }: { title: string }) => {
     <div
       className="Token flex items-center gap-2 bg-white py-[10px] px-[14px] rounded-[16px]"
     >
-      <div className="w-[48px] h-[48px] bg-text/10 rounded-full" />
+      <div className="w-[48px] h-[48px] flex items-center justify-center bg-gradient-to-b from-text/20 to-text/40 rounded-full">
+        <span className="text-bg text-[18px] leading-[40px] font-bold">{`${title.slice(-1)}${title.slice(0,1)}`}</span>
+      </div>
       <div className="flex-1">
         <div className="text-[18px] leading-[22px] font-medium">{title}</div>
         <div className="text-[14px] leading-[18px] text-[#3C3C4399]">850.9k / 100.3k</div>
--- src/pages/Watchlist.tsx
+++ src/pages/Watchlist.tsx
@@ -1,11 +1,30 @@
+import { useState } from 'react'
+
 import Page from '../kit/Page'
 import Menu from '../kit/Menu'
+import SearchInput from '../kit/SearchInput'
+import Tokens from '../kit/Tokens'
+
+import { useSearch, useMock } from '../hooks'
 
 function Watchlist() {
+  const [search, setSearch] = useState('')
+
+  const { tokens } = useMock()
+
+  const { tokensFiltered } = useSearch({ search, tokens: tokens.slice(0, 3) })
 
   return (
     <Page bottom={<Menu />}>
-      Soon...
+      <div className="py-[10px]">
+        <SearchInput
+          placeholder="Search ticker, name or address"
+          value={search}
+          onChange={setSearch}
+        />
+      </div>
+
+      <Tokens tokens={tokensFiltered} />
     </Page>
   )
 }
