1a817893f8a92c51ca822cac92418e27e56c9266
iower <iower@tutanota.com>
2024-06-23 01:29:20 +0500

    Improve Swap

--- src/pages/Swap.tsx
+++ src/pages/Swap.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { useSearch, useMock } from '../hooks'
 
@@ -12,6 +12,7 @@ import { TToken } from '../types'
 
 import { ReactComponent as AddIcon } from '../assets/add.svg'
 import { ReactComponent as SwapIcon } from '../assets/swap.svg'
+import TokenAvatar from '../kit/TokenAvatar'
 
 function Swap() {
   const [step, setStep] = useState<'START' | 'SELECT_FROM' | 'SELECT_TO'>('START')
@@ -36,32 +37,62 @@ function Swap() {
   const [search, setSearch] = useState('')
   const { tokensFiltered, isNotFound } = useSearch({ search, tokens })
 
+  useEffect(() => {
+    if (step === 'START') {
+      setSearch('')
+    }
+  }, [step])
+
   return (
     <Page>
       {step === 'START' && (
         <>
           <div className="flex flex-col gap-2">
             <div className="relative rounded-[20px] bg-white">
-              <Button
-                onClick={() => { setStep('SELECT_FROM') }}
-                className="flex items-center gap-2 p-[14px]"
-              >
-                {fromToken === null && (
-                  <>
-                    <div className="flex items-center justify-center h-[48px] w-[48px] rounded-full bg-main/10">
-                      <AddIcon className="w-6 h-6 text-main" />
+              <div className="relative">
+                <Button
+                  onClick={() => { setStep('SELECT_FROM') }}
+                  className="p-[14px] text-left"
+                >
+                  {!fromToken && (
+                    <div className="flex items-center gap-2">
+                      <div className="flex items-center justify-center h-[48px] w-[48px] rounded-full bg-main/10">
+                        <AddIcon className="w-6 h-6 text-main" />
+                      </div>
+                      <div className="text-[18px] leading-[22px] font-medium text-main">Swap from</div>
                     </div>
-                    <div className="text-[18px] leading-[22px] font-medium text-main">Swap from</div>
-                  </>
+                  )}
+                  {!!fromToken && (
+                    <div className="flex items-center justify-between gap-4">
+                      <div className="flex items-center gap-2">
+                        <TokenAvatar token={fromToken} />
+                        <div>
+                          <div className="text-[18px] leading-[22px] font-medium">{fromToken.title}</div>
+                          <div className="text-[14px] leading-[22px] text-text/40">0 {fromToken.ticker}</div>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </Button>
+                {!!fromToken && (
+                  <div className="absolute right-[14px] top-[50%] -translate-y-[50%]">
+                    <Button
+                      theme="small-light"
+                      onClick={() => { }}
+                    >
+                      Use Max
+                    </Button>
+                  </div>
                 )}
-              </Button>
+              </div>
+
               <div className="p-5 pb-8 border-black/[8%] border-t">
                 <InputAmount
                   amount={amount}
                   onChange={setAmount}
                 />
               </div>
-              <div className="absolute w-10 h-10 -bottom-[24px] left-[50%] -translate-x-[50%]">
+              <div className="z-[1] absolute w-10 h-10 -bottom-[24px] left-[50%] -translate-x-[50%]">
                 <Button
                   wrapperClassName=""
                   className="flex items-center justify-center w-10 h-10 bg-[#EAEAEC] rounded-full"
@@ -74,15 +105,26 @@ function Swap() {
             <div className="rounded-[20px] bg-white">
               <Button
                 onClick={() => { setStep('SELECT_TO') }}
-                className="flex items-center gap-2 p-[14px]"
+                className="p-[14px] text-left"
               >
-                {toToken === null && (
-                  <>
+                {!toToken && (
+                  <div className="flex items-center gap-2">
                     <div className="flex items-center justify-center h-[48px] w-[48px] rounded-full bg-main/10">
                       <AddIcon className="w-6 h-6 text-main" />
                     </div>
                     <div className="text-[18px] leading-[22px] font-medium text-main">Swap to</div>
-                  </>
+                  </div>
+                )}
+                {!!toToken && (
+                  <div className="flex items-center justify-between gap-4">
+                    <div className="flex items-center gap-2">
+                      <TokenAvatar token={toToken} />
+                      <div>
+                        <div className="text-[18px] leading-[22px] font-medium">{toToken.title}</div>
+                        <div className="text-[14px] leading-[22px] text-text/40">Receive {toToken.ticker}</div>
+                      </div>
+                    </div>
+                  </div>
                 )}
               </Button>
             </div>
