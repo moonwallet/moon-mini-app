4a3a5890cf0536d1506983bcc185db801c0cedb5
iower <iower@tutanota.com>
2024-07-18 14:50:35 +0500

    format.short

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -5,4 +5,23 @@ export const format = {
   address: (address: string) => `${address.substring(0, 4)}..${address.substring(address.length - 4)}`,
   percent: (decimalShare: number) => (decimalShare * 100).toFixed(2) + '%',
   commas: (amount: number) => parseFloat(amount.toFixed(2)).toLocaleString('en-US'),
+  short: (amount: number, digits: number = 2): string => {
+    const lookup = [
+      { value: 1, symbol: "" },
+      { value: 1e3, symbol: "k" },
+      { value: 1e6, symbol: "M" },
+      { value: 1e9, symbol: "G" },
+      { value: 1e12, symbol: "T" },
+      { value: 1e15, symbol: "P" },
+      { value: 1e18, symbol: "E" }
+    ]
+    try {
+      const regexp = /\.0+$|(\.[0-9]*[1-9])0+$/
+      const item = lookup.slice().reverse().find(item => amount >= item.value)
+      return item ? (amount / item.value).toFixed(digits).replace(regexp, '$1').concat(item.symbol) : '0'
+    } catch (e) {
+      console.error(e)
+    }
+    return '0'
+  }
 }
