3693dc73628e1856e4608a0799cba28919796370
iower <iower@tutanota.com>
2024-11-27 23:39:45 +0500

    Improve Dropdown

--- src/kit/Dropdown.tsx
+++ src/kit/Dropdown.tsx
@@ -1,53 +1,64 @@
 import cx from 'classnames'
+import { useState, ReactNode } from 'react'
 import OutsideClickHandler from 'react-outside-click-handler'
 
 import { Button } from '../kit'
 
 import { ReactComponent as CheckIcon } from '../assets/check.svg'
 
-export const Dropdown = ({ isOpen, setIsOpen, items, activeItem, right, onSelect }: {
-  isOpen: boolean,
-  setIsOpen: (_: boolean) => void
+export const Dropdown = ({ children, items, activeItem, right, onSelect }: {
+  children: ReactNode
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   items: any[]
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   activeItem: any
   right: boolean
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   onSelect: (_: any) => void
 }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <OutsideClickHandler
-      onOutsideClick={() => {
-        setIsOpen(false)
-      }}
-    >
-      <div
-        className={cx(
-          'Dropdown absolute mt-2 bg-[#f2f2f2] rounded-[12px] origin-top transition-all',
-          right ? 'right-0' : 'left-0',
-          isOpen ? 'opacity-100 scale-y-100' : 'opacity-0 scale-y-0',
-        )}
-        style={{ boxShadow: '0px 0px 32px 0px #00000033' }}
+    <div className="Dropdown relative">
+      <OutsideClickHandler
+        onOutsideClick={() => { setIsOpen(false) }}
       >
-        {items.map((_, i, arr) => (
-          <>
-            <Button
-              className="relative min-w-[82px] h-[44px] pl-8 pr-4 text-[17px] leading-[22px] text-left"
-              onClick={() => {
-                onSelect(_)
-                setIsOpen(false)
-              }}
-            >
-              {_}
-              <CheckIcon className={cx(
-                'absolute top-[50%] -translate-y-[50%] left-[10px] w-4 h-4 transition-all',
-                activeItem === _ ? 'opacity-100' : 'opacity-0',
-              )} />
-            </Button>
-            {i < arr.length - 1 && (
-              <div className="h-[0.5px] bg-[#11111140]" />
-            )}
-          </>
-        ))}
-      </div>
-    </OutsideClickHandler>
+        <Button
+          className="Dropdown-button"
+          onClick={() => { setIsOpen(!isOpen) }}
+        >
+          {children}
+        </Button>
+        <div
+          className={cx(
+            'Dropdown-list z-[1] absolute mt-2 bg-[#f2f2f2] rounded-[12px] origin-top transition-all',
+            right ? 'right-0' : 'left-0',
+            isOpen ? 'opacity-100 scale-y-100' : 'opacity-0 scale-y-0',
+          )}
+          style={{ boxShadow: '0px 0px 32px 0px #00000033' }}
+        >
+          {items.map((_, i, arr) => (
+            <>
+              <Button
+                className="relative min-w-[82px] h-[44px] pl-8 pr-4 text-[17px] leading-[22px] text-left"
+                onClick={() => {
+                  onSelect(_)
+                  setIsOpen(false)
+                }}
+              >
+                {_}
+                <CheckIcon className={cx(
+                  'absolute top-[50%] -translate-y-[50%] left-[10px] w-4 h-4 transition-all',
+                  activeItem === _ ? 'opacity-100' : 'opacity-0',
+                )} />
+              </Button>
+              {i < arr.length - 1 && (
+                <div className="h-[0.5px] bg-[#11111140]" />
+              )}
+            </>
+          ))}
+        </div>
+      </OutsideClickHandler>
+    </div>
   )
 }
--- src/kit/TokensHeader.tsx
+++ src/kit/TokensHeader.tsx
@@ -1,35 +1,29 @@
 import { useState } from 'react'
 
-import { Button, Dropdown } from '../kit'
+import { Dropdown } from '../kit'
 
 import { ReactComponent as SortIcon } from '../assets/sort.svg'
 
 export const TokensHeader = () => {
-  const [isPeriodOpen, setIsPeriodOpen] = useState(false)
-
   const periods = ['1h', '3h', '6h', '12h', '24h']
   const [period , setPeriod] = useState(periods[0])
 
   return (
     <div className="TokensHeader flex items-center justify-between">
       <div className="text-[14px] leading-[19px] text-[#3C3C4399] font-nu">Market Cap / Liquidity</div>
-      <div className="relative">
-        <Button
+      <Dropdown
+        right
+        items={periods}
+        activeItem={period}
+        onSelect={setPeriod}
+      >
+        <div
           className="flex items-center gap-1 text-[14px] leading-[19px] text-[#3C3C4399] font-nu"
-          onClick={() => { setIsPeriodOpen(true) }}
         >
           <span>Price / {period} change</span>
           <SortIcon className="w-[14px] h-[22px]" />
-        </Button>
-        <Dropdown
-          right
-          isOpen={isPeriodOpen}
-          setIsOpen={setIsPeriodOpen}
-          items={periods}
-          activeItem={period}
-          onSelect={setPeriod}
-        />
-      </div>
+        </div>
+      </Dropdown>
     </div>
   )
 }
